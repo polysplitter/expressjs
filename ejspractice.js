114ed9d294d031dc01cf88aeb814dcda046c9d74
Kyle <polysplitter@gmail.com>
2018-12-28 21:40:24 -0600

    updates

--- ejspractice.js
+++ ejspractice.js
@@ -13,8 +13,22 @@ app.use(express.urlencoded())
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// app.get('/', (req, res, next) => {
+//     res.render("index")
+// })
+
+function validateUser(req, res, next) {
+    res.locals.validated = true;
+    next()
+}
+
+app.use(validateUser)
+
 app.get('/', (req, res, next) => {
-    res.render("index")
+    res.render("index", {
+        msg: "Success!",
+        msg2: "Failure!"
+    })
 })
 
 app.listen(3000, () => {
